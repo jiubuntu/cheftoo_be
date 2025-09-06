934eec7926e3b78b25484001a4d8ff53473c11c0
jiubuntu <rlawldn0412@gmail.com>
2025-09-06 19:28:06 +0900

    chore: youtube videoId수집 스케줄러 테스트 원상복구

--- src/main/java/jwhs/cheftoo/youtube/scheduler/YoutubeScheduler.java
+++ src/main/java/jwhs/cheftoo/youtube/scheduler/YoutubeScheduler.java
@@ -13,8 +13,8 @@ public class YoutubeScheduler {
     private final YoutubeService youTubeService;
 
     // 매주 월요일 오전 3시
-//    @Scheduled(cron = "0 0 3 ? * MON")
-    @Scheduled(fixedDelay = 10000) // 테스트용
+    @Scheduled(cron = "0 0 3 ? * MON")
+//    @Scheduled(fixedDelay = 10000) // 테스트용
     public void updateYouTubeCacheWeekly() {
         log.info("Youtube VideoId 갱신 스케쥴러 시작");
         youTubeService.fetchAndCacheYouTubeVideos();
